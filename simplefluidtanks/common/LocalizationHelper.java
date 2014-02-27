42e6f7e1e49f25458a1d412f5dc71363517ae64c
zarathul <idont@think.so>
2014-02-27 16:18:26 +0100

    - forgot to add LocalizationHelper

---
+++ simplefluidtanks/common/LocalizationHelper.java
@@ -0,0 +1,39 @@
+package simplefluidtanks.common;
+
+import java.util.ArrayList;
+
+import net.minecraft.util.StatCollector;
+
+/**
+ * Provides helper methods for retrieving localized strings
+ */
+public final class LocalizationHelper
+{
+	/**
+	 * Gets the localized formatted strings for the specified key and formatting arguments.
+	 * @param key
+	 * The base key without an index (e.g. "myKey" gets "myKey0", "myKey1" ... etc.).
+	 * @param args
+	 * Formatting arguments.
+	 * @return
+	 */
+	public static ArrayList<String> multiLineTranslateToLocal(String key, Object ... args)
+	{
+		ArrayList<String> lines = new ArrayList<String>();
+		
+		if (key != null)
+		{
+			int x = 0;
+			String currentKey = key + x;
+			
+			// func_94522_b checks if the specified key exists
+			while (StatCollector.func_94522_b(currentKey))
+			{
+				lines.add(StatCollector.translateToLocalFormatted(currentKey, args));
+				currentKey = key + ++x;
+			}
+		}
+		
+		return lines;
+	}
+}
