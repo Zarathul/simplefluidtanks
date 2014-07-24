4f4450943ef7b3945f01b23e591cb166336de392
zarathul <idont@think.so>
2014-07-24 17:10:15 +0200

    - Updated WAILA API. - Fixed deprecation warning in ValveBlockDataProvider.

--- src/main/java/net/zarathul/simplefluidtanks/waila/ValveBlockDataProvider.java
+++ src/main/java/net/zarathul/simplefluidtanks/waila/ValveBlockDataProvider.java
@@ -76,7 +76,7 @@ public final class ValveBlockDataProvider implements IWailaDataProvider
 
 					if (fluid != null)
 					{
-						fluidName = fluid.getLocalizedName();
+						fluidName = fluid.getLocalizedName(fluidStack);
 					}
 				}
 
