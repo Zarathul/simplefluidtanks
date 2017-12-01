4d8b87b5d0e63d3f66bf4b18967313d95a351624
Zarathul <Zarathul@users.noreply.github.com>
2017-12-01 20:41:24 +0100

    - Updated to latest recommended Forge 1.12.2-14.23.1.2555. - Replaced wrong package-info for cofh|item API (Fixes: "Found mod(s) [simplefluidtanks] containing declared API package cofh.api.item (owned by cofhapi) without associated API reference"). - Replaced Waila with TheOneProbe soft dependency.

---
+++ src/main/java/cofh/api/item/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * (C) 2014-2017 Team CoFH / CoFH / Cult of the Full Hub
+ * http://www.teamcofh.com
+ */
+@API (apiVersion = "2.2.0", owner = "cofhapi", provides = "cofhapi|item")
+package cofh.api.item;
+
+import net.minecraftforge.fml.common.API;
